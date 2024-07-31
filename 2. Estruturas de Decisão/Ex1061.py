229567c7f43ea865d500b82af863d26667460b93
AlexTheHedgehog <69598508+AlexTheHedgehog@users.noreply.github.com>
2024-07-31 10:18:07 -0500

    Update Ex1061.py

--- 2. Estruturas de Decisão/Ex1061.py
+++ 2. Estruturas de Decisão/Ex1061.py
@@ -1,24 +1,19 @@
-dia1 = int(input('Dia '))
-h1, m1, s1 = map(int, input().replace(':', '').split())
-dia2 = int(input('Dia '))
-h2, m2, s2 = map(int, input().replace(':', '').split())
+dia_inicio = input().split()
+dia1 = int(dia_inicio[1])
+h1, m1, s1 = map(int, input().split(' : '))
+dia_fim = input().split()
+dia2 = int(dia_fim[1])
+h2, m2, s2 = map(int, input().split(' : '))
 
-dias = dia2 - dia1
-horas = minutos = segundos = 0
+inicio = (dia1 * 86400 + h1 * 3600 + m1 * 60 + s1)
+fim = (dia2 * 86400 + h2 * 3600 + m2 * 60 + s2)
+diff = fim - inicio
 
-if h2 > h1:
-    horas = h2 - h1
-else:
-    horas = 24 - (h1 - h2)
-
-if m2 > m1:
-    minutos = m2 - m1
-else:
-    minutos = 60 - (m1 - m2)
-
-if s2 > s1:
-    segundos = s2 - s1
-else:
-    segundos = 60 - (s1 - s2)
+dias = diff // 86400
+diff = diff % 86400
+horas = diff // 3600
+diff = diff % 3600
+minutos = diff // 60
+segundos = diff % 60
 
 print(f'{dias} dia(s)\n{horas} hora(s)\n{minutos} minuto(s)\n{segundos} segundo(s)')
